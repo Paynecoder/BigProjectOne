27c2a2d790e5e7da83f4973d609a17544d668b11
Aaron Tripp <aarontripp83@gmail.com>
2023-03-24 10:05:32 -0700

    fix global colours

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -57,11 +57,6 @@
 /******/ 	__webpack_require__.i = [];
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	!function() {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	}();
-/******/ 	
 /******/ 	/* webpack/runtime/chunk loaded */
 /******/ 	!function() {
 /******/ 		var deferred = [];
@@ -136,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "95278eb3e1a7c873"; }
+/******/ 		__webpack_require__.h = function() { return "3ec935948702cece"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
