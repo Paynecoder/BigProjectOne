feeebc9130be8ce7a9d7680982201bfb3767f80a
Paynecoder <joshpayne03@icloud.com>
2023-03-27 14:24:14 -0700

    quiz page

--- data/questions.js
+++ data/questions.js
@@ -3,21 +3,22 @@ export const questions = [
     id: 1,
     title: "Question 1",
     question: "Do you recycle your drink containers?",
-    image: "../public/images/questions/1.svg",
+    image: "/questions/1.svg",
     answers: ["Yes", "No"],
+    correctAnswer: "Yes",
   },
   {
     id: 2,
     title: "Question 2",
     question: "If you see garbage on the ground, do you pick it up?",
-    image: "../public/images/questions/2.svg",
+    image: "/questions/2.svg",
     answers: ["Yes", "No"],
   },
   {
     id: 3,
     title: "Question 3",
     question: "How do you commute to school/work?",
-    image: "../public/images/questions/3.svg",
+    image: "/questions/3.svg",
     answers: ["Walk/Bike", "Transit", "Drive"],
   },
   {
@@ -25,14 +26,14 @@ export const questions = [
     title: "Question 4",
     question:
       "Do you turn off lights and devices when you leave your home for the day?",
-    image: "../public/images/questions/4.svg",
+    image: "/questions/4.svg",
     answers: ["Yes", "No"],
   },
   {
     id: 5,
     title: "Question 5",
     question: "If you have food waste, do you use a compost bin?",
-    image: "../public/images/questions/5.svg",
+    image: "/questions/5.svg",
     answers: ["Yes", "No"],
   },
 ];
---
+++ pages/quiz.js
@@ -0,0 +1,38 @@
+import styles from "../styles/Quiz.module.css";
+import { useState, useEffect } from "react";
+import { questions } from "../data/questions.js";
+import Navbar from "../components/Navbar/index.js";
+import Image from "next/image";
+import Button from "../components/Button/index.js";
+
+export default function Quiz() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [quizData, setQuizData] = useState([...questions]);
+  const [currentQuestionData, setCurrentQuestionData] = useState({});
+
+  let score = 0;
+
+  useEffect(() => {
+    setCurrentQuestionData(quizData[currentQuestion]);
+  }, [currentQuestion]);
+
+  return (
+    <>
+      <Navbar />
+      <main className={styles.main}>
+        <div>
+          <Image src={currentQuestionData.image} width={225} height={225} />
+        </div>
+        <div className={styles.qtitlebox}>
+          <h1 className={styles.title}>{currentQuestionData.title}</h1>
+          <p>{currentQuestionData.question}</p>
+        </div>
+        <div>
+          {currentQuestionData.answers?.map((answer, index) => (
+            <Button key={index} word={answer} route={""} />
+          ))}
+        </div>
+      </main>
+    </>
+  );
+}
