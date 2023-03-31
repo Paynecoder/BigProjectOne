072db43eaa8f6069ef03421b13d9b5824c893b62
Aaron Tripp <aarontripp83@gmail.com>
2023-03-31 12:42:58 -0700

    add button slider

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a11981669fe4c9f5"; }
+/******/ 		__webpack_require__.h = function() { return "5354e24836aafd6b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
