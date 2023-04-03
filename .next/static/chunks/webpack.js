d563feb8091ccbbd0ab3712ac90727831b3317ee
Aaron Tripp <aarontripp83@gmail.com>
2023-04-03 14:16:42 -0700

    mobile fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5957041e3aedb9fe"; }
+/******/ 		__webpack_require__.h = function() { return "e3120aff7e12b380"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
