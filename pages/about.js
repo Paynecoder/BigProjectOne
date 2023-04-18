4e9fcbd8a16fd8b07f56ea269d02d580ee9b9597
Aaron Tripp <aarontripp83@gmail.com>
2023-04-18 09:31:23 -0700

    update team members

--- pages/about.js
+++ pages/about.js
@@ -17,42 +17,40 @@ export default function About() {
       <Navbar />
       <main className={styles.main}>
         <div className={styles.title_head}>
-        <h1 style={{ fontWeight: 900 }}>Meet the Team!</h1>
+          <h1 style={{ fontWeight: 900 }}>Meet the Team!</h1>
         </div>
         <div className={styles.team_member}>
           <Image src={aaron} alt="Team Member 1" width={100} />
           <div>
-            <h3>🎨 Aaron - The Artful Illusionist 🎨</h3>
+            <h3>🎨 Aaron - Visual Artist</h3>
             <p>
-              Behold Aaron, our in-house artistic illusionist, who dazzles with
-              his ability to transmute even the dullest concepts into visually
-              captivating masterpieces. With a stroke of his brush (or stylus),
-              Aaron breathes life into our app, creating vibrant worlds and
-              playful characters.
+              Behold Aaron Tripp, our primary visual designer. Aaron is the
+              creator of all character icons, illustrations, and animations that
+              bring Brrrilliant to life!
             </p>
           </div>
         </div>
         <div class={styles.team_member}>
           <Image src={carter} alt="Team Member 2" width={100} />
           <div>
-            <h3>🔮 Carter - The UX/UI Enchanter 🔮</h3>
+            <h3>🔮 Carter - UI/UX Specialist</h3>
             <p>
-              Meet Carter, the UX/UI Enchanter who mesmerizes with seamless
-              navigation and delightful user experiences. With a flick of his
-              wand (and a dash of user feedback), Carter ensures that your
-              journey is as intuitive as it is enjoyable.
+              Meet Carter Vandekamp, our head UI/UX designer. Carter is
+              responsible for discovering exactly what features are key to
+              enhancing our page navigation and layout to create the most
+              delightful user experience.
             </p>
           </div>
         </div>
         <div class={styles.team_member}>
           <Image src={josh} alt="Team Member 3" width={100} />
           <div>
-            <h3>🌟 Joshua - The Code Magician 🌟</h3>
+            <h3>🌟 Joshua - Frontend Wizard</h3>
             <p>
-              Introducing Joshua, the mystical maestro of ones and zeros! With
-              the finesse of a master programmer, he artfully transforms
-              caffeine into code, weaving complex spells into our app's very
-              essence.
+              Introducing Joshua Payne. If you can think it, he can code it.
+              Joshua is the driving force that helps us implement advanced
+              interactive features such as AI integration and reliable,
+              efficient code.
             </p>
           </div>
         </div>
