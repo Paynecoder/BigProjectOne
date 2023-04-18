d18eece3bc200cc2f61d10493f95dc04bd90e6c1
Aaron Tripp <aarontripp83@gmail.com>
2023-04-18 13:30:12 -0700

    modern shiny button

--- components/ShinyButton/index.js
+++ components/ShinyButton/index.js
@@ -16,8 +16,22 @@ export default function Button({ word, route }) {
         }, 300);
       };
       button.addEventListener("click", buttonClicked);
+      const handleMouseMove = (event) => {
+        const button = buttonRef.current;
+        if (button) {
+          const rect = button.getBoundingClientRect();
+          const centerX = rect.left + rect.width / 2;
+          const centerY = rect.top + rect.height / 2;
+          const x = (event.clientX - centerX) / (rect.width / 2);
+          const y = (event.clientY - centerY) / (rect.height / 2);
+          button.style.backgroundPosition = `${50 + x * 15}% ${50 + y * 15}%`;
+        }
+      };
+
+      button.addEventListener("mousemove", handleMouseMove);
       return () => {
         button.removeEventListener("click", buttonClicked);
+        button.removeEventListener("mousemove", handleMouseMove);
       };
     }
   }, [buttonRef.current]);
